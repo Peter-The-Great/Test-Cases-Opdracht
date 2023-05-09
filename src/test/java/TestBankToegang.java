bf726343db4e529b400142be26dfee4eb9987dd8
Peter-The-Great <pjotrw15@gmail.com>
2023-05-09 21:32:34 +0200

    Heb de gedeeltes toegevoegd die te maken hebben met Equivelentie klassen en pairwise testing.

--- src/test/java/TestBankToegang.java
+++ src/test/java/TestBankToegang.java
@@ -108,4 +108,71 @@ public class TestBankToegang {
         // Test case 8: isbaas en ismanager zijn onwaar, jareninDienst is meer dan 8
         assertFalse(Bank.getToegangVault(false, false, 10));
     }
+
+    /**
+     * Test de toegang voor de bank met een all-uses coverage test
+     */
+    @Test
+    public void TestBankToegangEquivelent(){
+        // Testen van geldige input
+        // Testen van isBaas
+        assertTrue(Bank.getToegangVault(true, false, 0)); // Equivalentieklasse: isbaas = true
+        assertFalse(Bank.getToegangVault(false, false, 0)); // Equivalentieklasse: isbaas = false, ismanager = false
+        assertFalse(Bank.getToegangVault(false, true, 7)); // Equivalentieklasse: isbaas = false, ismanager = true, jareninDienst < 8
+        assertFalse(Bank.getToegangVault(false, true, 8)); // Equivalentieklasse: isbaas = false, ismanager = true, jareninDienst = 8
+        assertTrue(Bank.getToegangVault(false, true, 9)); // Equivalentieklasse: isbaas = false, ismanager = true, jareninDienst > 8
+
+        // Testen van ongeldige input
+        assertFalse(Bank.getToegangVault(false, true, -1)); // Equivalentieklasse: jareninDienst < 0
+    }
+    /**
+     * Test de toegang voor de bank met een boundary coverage test
+     */
+    @Test
+    public void TestBankToegangRandwaarden(){
+        //Testcase 1: baas
+        assertTrue(Bank.getToegangVault(true, false, 0));
+
+        //Testcase 2: manager, jareninDienst > 8
+        assertTrue(Bank.getToegangVault(false, true, 9));
+
+        //Testcase 3: manager, jareninDienst == 8
+        assertFalse(Bank.getToegangVault(false, true, 8));
+
+        //Testcase 4: manager, jareninDienst < 8
+        assertFalse(Bank.getToegangVault(false, true, 7));
+
+        //Testcase 5: klant
+        assertFalse(Bank.getToegangVault(false, false, 0));
+    }
+
+    /**
+     * Test de toegang voor de bank met een pairwise coverage test
+     */
+    @Test
+    public void TestBankToegangPairwise(){
+        // Test case 1: isbaas = false, ismanager = false, jareninDienst = 0
+        assertFalse(Bank.getToegangVault(false, false, 0));
+
+        // Test case 2: isbaas = false, ismanager = true, jareninDienst = 1
+        assertFalse(Bank.getToegangVault(false, true, 1));
+
+        // Test case 3: isbaas = false, ismanager = true, jareninDienst = 8
+        assertFalse(Bank.getToegangVault(false, true, 8));
+
+        // Test case 4: isbaas = false, ismanager = true, jareninDienst = 9
+        assertTrue(Bank.getToegangVault(false, true, 9));
+
+        // Test case 5: isbaas = true, ismanager = false, jareninDienst = 0
+        assertTrue(Bank.getToegangVault(true, false, 0));
+
+        // Test case 6: isbaas = true, ismanager = true, jareninDienst = 1
+        assertTrue(Bank.getToegangVault(true, true, 1));
+
+        // Test case 7: isbaas = true, ismanager = true, jareninDienst = 8
+        assertTrue(Bank.getToegangVault(true, true, 8));
+
+        // Test case 8: isbaas = true, ismanager = true, jareninDienst = 9
+        assertTrue(Bank.getToegangVault(true, true, 9));
+    }
 }
